7678e15502119b351b89d94f2f8d0c1e67887c46
askmesupu <ask.me.pirated@gmail.com>
2025-09-15 19:46:46 +0600

    Update MyHeart.js

--- src/pages/MyHeart.js
+++ src/pages/MyHeart.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+const words = Array.from({ length: 40 }, () => "Shiropa");
+
 export default function MyHeart() {
-  const words = Array.from({ length: 100 }, () => "Shiropa");
   return (
-    <div className="w-full min-h-screen bg-gradient-to-r from-pink-400 via-purple-400 to-blue-400 bg-animate relative overflow-hidden">
+    <div className="w-full min-h-screen overflow-hidden bg-gradient-to-r from-pink-400 via-purple-400 to-blue-400 bg-animate relative flex items-center justify-center">
       {words.map((word, idx) => (
         <motion.span
           key={idx}
-          initial={{ x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight, opacity: 0 }}
+          initial={{ x: Math.random() * window.innerWidth, y: 600, opacity: 0 }}
           animate={{ y: -50, opacity: 1 }}
-          transition={{ duration: 10 + Math.random() * 10, repeat: Infinity }}
-          className="absolute text-white font-bold"
+          transition={{ duration: 12 + Math.random() * 6, repeat: Infinity, ease: "linear" }}
+          className="absolute text-white font-bold text-sm md:text-lg"
         >
           {word}
         </motion.span>
       ))}
     </div>
   );
-                   }
+            }
