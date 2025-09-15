b189fd05152fd0bbb1d4e96764f8775846e66fc3
askmesupu <ask.me.pirated@gmail.com>
2025-09-15 22:03:47 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MyLoveEnglish from "./pages/MyLoveEnglish";
 import MyLoveBangla from "./pages/MyLoveBangla";
 import MyLoveHindi from "./pages/MyLoveHindi";
 import MyHeart from "./pages/MyHeart";
-import AnswerMyQuestion from "./pages/AnswerMyQuestion";
+import AnswerQuestion from "./pages/AnswerQuestion";
+import "./index.css";
 
-export default function App() {
+function App() {
+  const [page, setPage] = useState("home");
+
   return (
-    <Router>
-      <div className="min-h-screen relative">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/myloveenglish" element={<MyLoveEnglish />} />
-          <Route path="/mylovebangla" element={<MyLoveBangla />} />
-          <Route path="/mylovehindi" element={<MyLoveHindi />} />
-          <Route path="/myheart" element={<MyHeart />} />
-          <Route path="/answermyquestion" element={<AnswerMyQuestion />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="bg-animate min-h-screen relative">
+      <header>
+        <h1>Supto ❤️ Shiropa</h1>
+      </header>
+      <Navbar setPage={setPage} />
+      {page === "home" && <Home />}
+      {page === "english" && <MyLoveEnglish />}
+      {page === "bangla" && <MyLoveBangla />}
+      {page === "hindi" && <MyLoveHindi />}
+      {page === "myheart" && <MyHeart />}
+      {page === "question" && <AnswerQuestion />}
+    </div>
   );
 }
+
+export default App;
