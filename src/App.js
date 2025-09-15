808bde3dbab16a15bb71f764448f894382e32cb4
askmesupu <ask.me.pirated@gmail.com>
2025-09-15 20:31:03 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MyLoveEnglish from "./pages/MyLoveEnglish";
 import MyLoveBangla from "./pages/MyLoveBangla";
 import MyLoveHindi from "./pages/MyLoveHindi";
 import MyHeart from "./pages/MyHeart";
 import AnswerMyQuestion from "./pages/AnswerMyQuestion";
-import Navbar from "./components/Navbar";
 
-function App() {
+export default function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/english" element={<MyLoveEnglish />} />
-        <Route path="/bangla" element={<MyLoveBangla />} />
-        <Route path="/hindi" element={<MyLoveHindi />} />
-        <Route path="/myheart" element={<MyHeart />} />
-        <Route path="/question" element={<AnswerMyQuestion />} />
-      </Routes>
+      <div className="min-h-screen bg-gradient-to-r from-purple-400 via-pink-400 to-blue-400">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/myloveenglish" element={<MyLoveEnglish />} />
+          <Route path="/mylovebangla" element={<MyLoveBangla />} />
+          <Route path="/mylovehindi" element={<MyLoveHindi />} />
+          <Route path="/myheart" element={<MyHeart />} />
+          <Route path="/answermyquestion" element={<AnswerMyQuestion />} />
+        </Routes>
+      </div>
     </Router>
   );
-}
-
-export default App;
+    }
