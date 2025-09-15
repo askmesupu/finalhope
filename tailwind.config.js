9770ba3282973aa444bebecb8b3f407aa50ac8ec
askmesupu <ask.me.pirated@gmail.com>
2025-09-15 18:53:09 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        gradient: 'gradientBG 15s ease infinite'
+      },
+      keyframes: {
+        gradientBG: {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '0% 50%' },
+        }
+      }
+    },
+  },
+  plugins: [],
+            }
