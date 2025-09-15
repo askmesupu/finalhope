a04d0e5fad4f796313fc307814fe8ab2e7bbe26b
askmesupu <ask.me.pirated@gmail.com>
2025-09-15 20:08:44 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import SubpageButton from "./SubpageButton";
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex flex-col md:flex-row justify-center items-center gap-4 md:gap-6 p-4 md:p-6">
+    <nav className="absolute top-0 left-0 w-full flex flex-col md:flex-row justify-center items-center gap-3 md:gap-6 p-4 z-20">
       <SubpageButton to="/my-love-english" label="My Love (English)" />
       <SubpageButton to="/my-love-bangla" label="My Love (Bangla)" />
       <SubpageButton to="/my-love-hindi" label="My Love (Hindi)" />
@@ -11,4 +11,4 @@ export default function Navbar() {
       <SubpageButton to="/answer-my-question" label="Answer My Question" />
     </nav>
   );
-}
+    }
